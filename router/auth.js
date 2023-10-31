071bf1d2f416efec6ba7ee7eab53b1423aa8fd3a
HarshJahajPuriya <harshkumar9826@gmail.com>
2023-10-31 17:31:45 +0530

    FEAT: Login and Get Logged in UserData endpoints added, with fetchUserIdToken middleware

--- router/auth.js
+++ router/auth.js
@@ -6,10 +6,11 @@ const { body, validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 const Error = require('../utils/Error');
 const JWT_SECRET = require('../utils/CONSTANTS');
+const fetchUserIdFromToken = require('../middleware/fetchUserIdFromToken');
 
-// Create a user using: POST "/api/auth/", Doesn't require Auth
+// ROUTE 1: Create a user using: POST "/api/auth/createuser", No Login Required
 router.post(
-  "/",
+  "/createUser",
   // Applying Validation
   [
     body("name").isLength({ min: 3 }).withMessage("Name requires atleast 3 characters"),
@@ -17,10 +18,10 @@ router.post(
     body("password").isLength({ min: 6 }).withMessage("Password must contain atleast 6 characters")
   ],
   async (req, res) => {
-    
+
     // If Validation Fails
     const resultErr = validationResult(req);
-    if (! resultErr.isEmpty()) {
+    if (!resultErr.isEmpty()) {
       let errors = resultErr['errors']
       res.status(422).send(errors.map(e => new Error(e.path, e.msg)));
       return;
@@ -30,7 +31,7 @@ router.post(
     try {
       // creating a user object using the model
       const user = new User(req.body);
-      
+
       // generating salt
       const salt = bcrypt.genSaltSync(10);
       // generating hash of the password using salt
@@ -51,8 +52,8 @@ router.post(
 
       // Generating authToken
       const authToken = jwt.sign(data, JWT_SECRET)
-      
-      res.send({authToken})
+
+      res.send({ authToken })
     } catch (err) {
       // error code will be 11000 if the unique index fails
       if (err.code === 11000) {
@@ -62,6 +63,73 @@ router.post(
         res.status(500).send(err);
       }
     }
-  })
+  }
+)
+
+
+// ROUTE 2: Authenticate a user using: POST "/api/auth/login", No Login Required
+router.post(
+  "/login",
+  // Applying Validation
+  [
+    body("email").isEmail().withMessage("Enter valid Email"),
+    body("password").isLength({ min: 6 }).withMessage("Password must contain atleast 6 characters")
+  ],
+  async (req, res) => {
+    
+    // If Validation Fails
+    const resultErr = validationResult(req);
+    if (! resultErr.isEmpty()) {
+      let errors = resultErr['errors']
+      res.status(422).send(errors.map(e => new Error(e.path, e.msg)));
+      return;
+    }
+
+    const {email, password} = req.body;
+
+    // Trying to fetch user by email
+    const user = await User.findOne({email: email})
+    
+    // If user does not exists with this email
+    if(!user) {
+      return res.status(400).send(new Error("Credentials", "Enter valid credentials"));
+    }
+    
+    let isPasswordValid = bcrypt.compareSync(password, user.password)
+    // If the password does not matches with the stored hash of that password
+    if(!isPasswordValid) {
+      return res.status(400).send(new Error("Credentials", "Enter valid credentials"));
+    }
+
+    // If everything is correct, creating object to put in the authToken
+    const data = {
+      user: {
+        id: user.id
+      }
+    }
+    // Generating authToken
+    const authToken = jwt.sign(data, JWT_SECRET);
+
+    res.send({authToken});
+  }
+)
+
+
+// ROUTE 3: Get logged in user data: POST "/api/auth/getuser", Login Required
+router.post(
+  "/getuser",
+  fetchUserIdFromToken,
+  async (req, res) => {
+    try {
+      let user = req.user;
+      user = await User.findById(user.id).select("-password")
+      res.send(user);
+    } catch(err) {
+      console.log(err)
+      res.status(500).send(new Error("getuser Endpoint", "Internal Server Error"));
+    }
+  }
+)
+
 
 module.exports = router;
